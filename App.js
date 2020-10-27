cc126050ec1f28612d43e0249052d598d541e9ac
Kyle Polson <kylepolson1993@gmail.com>
2020-10-27 14:54:19 -0700

    Added Loading and Error Messages

--- App.js
+++ App.js
@@ -3,13 +3,17 @@
 //import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import Main from './components/MainComponent';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 
 export default function App() {
   return (
-    
+    <Provider store={store}>
       <Main />
-    
+    </Provider>
   );
 }
 
